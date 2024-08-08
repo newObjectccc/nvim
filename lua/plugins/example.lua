65d204e6dddfc2784d4632583801645dd23f8393
newObjectccc <897333949@qq.com>
2024-08-08 10:34:46 +0800

    chore: configuration fix done

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -64,37 +64,37 @@ return {
   },
 
   -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       -- pyright will be automatically installed with mason and loaded with lspconfig
+  --       pyright = {},
+  --     },
+  --   },
+  -- },
 
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+  -- { import = "lazyvim.plugins.extras.lang.typescript" },
 
   -- add more treesitter parsers
 
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-      })
-    end,
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     -- add tsx and treesitter
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "tsx",
+  --       "typescript",
+  --     })
+  --   end,
+  -- },
 
   -- the opts function can also be used to change the default opts:
   {
@@ -102,18 +102,6 @@ return {
     event = "VeryLazy",
     opts = function(_, opts)
       table.insert(opts.sections.lualine_x, "😄")
-    end,
-  },
-
-  -- or you can return new options to override all the defaults
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    opts = function()
-      return {
-        --[[add your custom lualine config here]]
-      }
     end,
   },
 
@@ -136,92 +124,78 @@ return {
     },
   },
 
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = true },
-      panel = { enabled = true },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-
-  {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
-    enabled = true,
-    init = false,
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local logo = [[
-         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
-         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-    ]]
-
-      dashboard.section.header.val = vim.split(logo, "\n")
-    -- stylua: ignore
-    dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find file",       LazyVim.pick()),
-      dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
-      dashboard.button("r", " " .. " Recent files",    LazyVim.pick("oldfiles")),
-      dashboard.button("g", " " .. " Find text",       LazyVim.pick("live_grep")),
-      dashboard.button("c", " " .. " Config",          LazyVim.pick.config_files()),
-      dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-      dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
-      dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
-      dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
-    }
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
-      end
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
-      dashboard.section.footer.opts.hl = "AlphaFooter"
-      dashboard.opts.layout[1].val = 8
-      return dashboard
-    end,
-    config = function(_, dashboard)
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          once = true,
-          pattern = "AlphaReady",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      require("alpha").setup(dashboard.opts)
-
-      vim.api.nvim_create_autocmd("User", {
-        once = true,
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          dashboard.section.footer.val = "⚡ Neovim loaded "
-            .. stats.loaded
-            .. "/"
-            .. stats.count
-            .. " plugins in "
-            .. ms
-            .. "ms"
-          pcall(vim.cmd.AlphaRedraw)
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "goolord/alpha-nvim",
+  --   event = "VimEnter",
+  --   enabled = true,
+  --   init = false,
+  --   opts = function()
+  --     local dashboard = require("alpha.themes.dashboard")
+  --     local logo = [[
+  --        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+  --        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
+  --        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
+  --        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+  --        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+  --        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
+  --   ]]
+  --
+  --     dashboard.section.header.val = vim.split(logo, "\n")
+  --   -- stylua: ignore
+  --   dashboard.section.buttons.val = {
+  --     dashboard.button("f", " " .. " Find file",       LazyVim.pick()),
+  --     dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
+  --     dashboard.button("r", " " .. " Recent files",    LazyVim.pick("oldfiles")),
+  --     dashboard.button("g", " " .. " Find text",       LazyVim.pick("live_grep")),
+  --     dashboard.button("c", " " .. " Config",          LazyVim.pick.config_files()),
+  --     dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+  --     dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
+  --     dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+  --     dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+  --   }
+  --     for _, button in ipairs(dashboard.section.buttons.val) do
+  --       button.opts.hl = "AlphaButtons"
+  --       button.opts.hl_shortcut = "AlphaShortcut"
+  --     end
+  --     dashboard.section.header.opts.hl = "AlphaHeader"
+  --     dashboard.section.buttons.opts.hl = "AlphaButtons"
+  --     dashboard.section.footer.opts.hl = "AlphaFooter"
+  --     dashboard.opts.layout[1].val = 8
+  --     return dashboard
+  --   end,
+  --   config = function(_, dashboard)
+  --     -- close Lazy and re-open when the dashboard is ready
+  --     if vim.o.filetype == "lazy" then
+  --       vim.cmd.close()
+  --       vim.api.nvim_create_autocmd("User", {
+  --         once = true,
+  --         pattern = "AlphaReady",
+  --         callback = function()
+  --           require("lazy").show()
+  --         end,
+  --       })
+  --     end
+  --
+  --     require("alpha").setup(dashboard.opts)
+  --
+  --     vim.api.nvim_create_autocmd("User", {
+  --       once = true,
+  --       pattern = "LazyVimStarted",
+  --       callback = function()
+  --         local stats = require("lazy").stats()
+  --         local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+  --         dashboard.section.footer.val = "⚡ Neovim loaded "
+  --           .. stats.loaded
+  --           .. "/"
+  --           .. stats.count
+  --           .. " plugins in "
+  --           .. ms
+  --           .. "ms"
+  --         pcall(vim.cmd.AlphaRedraw)
+  --       end,
+  --     })
+  --   end,
+  -- },
 
   {
     "MagicDuck/grug-far.nvim",
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -21,6 +21,13 @@ return {
     local cmp = require("cmp")
     local defaults = require("cmp.config.default")()
     local auto_select = true
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    end
     return {
       auto_brackets = {}, -- configure any filetype to auto add brackets
       completion = {
@@ -40,6 +47,8 @@ return {
         end,
       }),
       sources = cmp.config.sources({
+        -- Copilot Source
+        { name = "copilot" },
         { name = "nvim_lsp" },
         { name = "path" },
       }, {
