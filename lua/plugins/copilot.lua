9d7810bb576b9aedc03043d98e8c16af6aa80b7b
newObjectccc <897333949@qq.com>
2024-08-30 01:01:51 +0800

    docs: remove unused

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,29 +4,6 @@ return {
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
-      -- panel = {
-      --   enabled = true,
-      --   auto_refresh = true,
-      --   keymap = {},
-      --   layout = {
-      --     position = "bottom", -- | top | left | right
-      --     ratio = 0.4,
-      --   },
-      -- },
-      -- suggestion = {
-      --   enabled = true,
-      --   auto_trigger = true,
-      --   hide_during_completion = true,
-      --   debounce = 75,
-      --   keymap = {
-      --     accept = "<Tab>",
-      --     accept_word = false,
-      --     accept_line = false,
-      --     next = "<M-]>",
-      --     prev = "<M-[>",
-      --     dismiss = "<C-]>",
-      --   },
-      -- },
       filetypes = {
         ["*"] = true,
       },
